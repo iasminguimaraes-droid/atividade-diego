f9a3d21064262a45ac220e773d2e4abe9f96369f
iasminguimaraes-droid <iasmin.fernandes@escola.pr.gov.br>
2026-06-06 19:55:15 -0300

    Atividade C3.c

--- atividades Diego/listaExercicios/C3.c
+++ atividades Diego/listaExercicios/C3.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
 
+float somar(float a, float b) {
+    return a + b;
+}
+
+float subtrair(float a, float b) {
+    return a - b;
+}
+
+float multiplicar(float a, float b) {
+    return a * b;
+}
+
+float dividir(float a, float b) {
+    if (b == 0) {
+        printf("Erro: divisao por zero!\n");
+        return 0;
+    }
+    return a / b;
+}
+
+int main() {
+    float num1, num2;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%f", &num1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%f", &num2);
+
+    printf("Soma = %.2f\n", somar(num1, num2));
+    printf("Subtracao = %.2f\n", subtrair(num1, num2));
+    printf("Multiplicacao = %.2f\n", multiplicar(num1, num2));
+
+    if (num2 != 0) {
+        printf("Divisao = %.2f\n", dividir(num1, num2));
+    } else {
+        dividir(num1, num2);
+    }
+
+}
