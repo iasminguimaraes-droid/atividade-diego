332edf83e42fa5e95f3ca0cdc8fe55cf1a02663a
iasminguimaraes-droid <iasmin.fernandes@escola.pr.gov.br>
2026-06-06 00:33:52 -0300

    atividade A2.c
    fiz uma função para exibir um placar

--- atividades Diego/listaExercicios/A2.c
+++ atividades Diego/listaExercicios/A2.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+struct Placar {
+    int timeA;
+    int timeB;
+};
+
+void main() {
+    printf("digite o placar do time A:");
+    struct Placar placar;
+    scanf("%i", &placar.timeA);
+
+    printf("digite o placar do time B:");
+    scanf("%i", &placar.timeB);
+
+    printf("placar do time A: %i\n", placar.timeA);
+    printf("placar do time B: %i\n", placar.timeB);
+
+    if(placar.timeA > placar.timeB) {
+        printf("time A venceu!\n");
+    } 
+    else if (placar.timeA == placar.timeB) {
+        printf("empatou\n");
+    }
+    else {
+        printf("time B venceu!\n");
+    }
+
+}
