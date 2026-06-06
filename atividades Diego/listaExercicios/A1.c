4f13192e26a800aee5d461a9b02cd85e0c30cde0
iasminguimaraes-droid <iasmin.fernandes@escola.pr.gov.br>
2026-06-05 23:56:01 -0300

    atividade A1.c

--- atividades Diego/listaExercicios/A1.c
+++ atividades Diego/listaExercicios/A1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+struct Pessoa {
+    char nome[50];
+    int idade;
+    float altura;
+};
+
+void main() {
+    struct Pessoa pessoa;
+
+    printf("Digite o nome da pessoa:");
+    scanf("%s", &pessoa.nome);
+
+    printf("Digite a idade da pessoa:");
+    scanf("%i", &pessoa.idade);
+
+    printf("Digite a sua altura:");
+    scanf("%f", &pessoa.altura);
+
+    printf("A pessoa registrada e:\nNome: %s\nIdade: %i\nAltura: %f", pessoa.nome, pessoa.idade, pessoa.altura);
+}
